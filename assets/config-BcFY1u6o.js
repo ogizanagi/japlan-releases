00965984e90bf5799d159ba324ac417f9257bc04
ogizanagi <ogizanagi@users.noreply.github.com>
2026-02-25 21:47:19 +0000

    deploy: ogizanagi/japlan@49ee1b6d08a33e94b55ad50499029a90cc061585

--- assets/config-BcFY1u6o.js
+++
@@ -1,1 +0,0 @@
-const s={build:{version:"0.0.1",commitHash:"9f6a0f9"},supabase:{url:"https://kmjhqhvdxqnbkcwjunrj.supabase.co",publishableKey:"sb_publishable_ysSUndrbJvEauZTIvjTN5Q_Uusj4O3L"}};export{s as c};
