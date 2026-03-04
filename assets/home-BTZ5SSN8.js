fa59290446dfbae2eeb9ae44d94f62a42c5c3c6d
ogizanagi <ogizanagi@users.noreply.github.com>
2026-03-04 21:07:59 +0000

    deploy: ogizanagi/japlan@4ae6e24ea145b92cae79a3d1f69778f1dd85525d

---
+++ assets/home-BTZ5SSN8.js
@@ -0,0 +1,1 @@
+import{h as o}from"./chunk-JPUPSTYD-CKp_vlI6.js";import{w as t,l as e,N as r}from"./chunk-LFPYN7LY-CA0Dqa3d.js";const p=t(function(){return e.jsx(r,{to:o("/map"),replace:!0})});export{p as default};
