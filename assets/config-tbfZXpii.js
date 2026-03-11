26ccd22e489c568215a8a00ffc39723a0daf1839
ogizanagi <ogizanagi@users.noreply.github.com>
2026-03-11 18:32:59 +0000

    deploy: ogizanagi/japlan@29f4fdbd9c8aa3ccb50a9841d88dd606ae45d694

---
+++ assets/config-tbfZXpii.js
@@ -0,0 +1,1 @@
+const s={build:{version:"0.0.1",commitHash:"29f4fdb"},supabase:{url:"https://kmjhqhvdxqnbkcwjunrj.supabase.co",publishableKey:"sb_publishable_ysSUndrbJvEauZTIvjTN5Q_Uusj4O3L",storageKey:"japlan-auth-token"}};export{s as c};
