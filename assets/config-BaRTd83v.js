2d383902b01406a6ff666bdb1e7360e2ef05b8d8
ogizanagi <ogizanagi@users.noreply.github.com>
2026-03-15 21:07:58 +0000

    deploy: ogizanagi/japlan@1384d295329fef3827cf5e9267f071efd8444da9

---
+++ assets/config-BaRTd83v.js
@@ -0,0 +1,1 @@
+const s={build:{version:"0.0.1",commitHash:"1384d29"},supabase:{url:"https://kmjhqhvdxqnbkcwjunrj.supabase.co",publishableKey:"sb_publishable_ysSUndrbJvEauZTIvjTN5Q_Uusj4O3L",storageKey:"japlan-auth-token"}};export{s as c};
