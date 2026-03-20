dc7e01d04c57b15d49b0895e4997dfbd3f15b2cf
ogizanagi <ogizanagi@users.noreply.github.com>
2026-03-20 12:55:45 +0000

    deploy: ogizanagi/japlan@d4d76a1f83bd99a5e1c59b7e8712451bafc5301d

--- assets/VersionTag-DGXKiRdP.js
+++
@@ -1,1 +0,0 @@
-import{l as t}from"./chunk-LFPYN7LY-CE9z5Z4Q.js";import{c as o}from"./config-BXhzotnl.js";import{c as e}from"./utils-BQHNewu7.js";function a({className:r,style:s}){return t.jsxs("span",{className:e("text-[10px] text-muted-foreground/50 select-none",r),style:s,children:["v",o.build.version," (",o.build.commitHash,")"]})}export{a as V};
