9915cbdf8264551acceba7c815a23183ebca2b2a
ogizanagi <ogizanagi@users.noreply.github.com>
2026-04-09 18:28:10 +0000

    deploy: ogizanagi/japlan@4c0b76c27b4b507c5b432b08f3bdd15bc7ec2622

---
+++ assets/VersionTag-Cy2oYdaA.js
@@ -0,0 +1,1 @@
+import{l as t}from"./chunk-LFPYN7LY-CE9z5Z4Q.js";import{a as s,c as n}from"./config-DwVkp6tr.js";function a({className:e,style:o}){return t.jsxs("span",{className:n("text-[10px] text-muted-foreground/50 select-none",e),style:o,children:["v",s.build.version," (",s.build.commitHash,")"]})}export{a as V};
