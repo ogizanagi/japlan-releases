0b19392765665a67b3ee54224aad32aa5ec19ebc
ogizanagi <ogizanagi@users.noreply.github.com>
2026-08-05 13:47:13 +0000

    deploy: ogizanagi/japlan@1daaa49f1e2999dcf2b783ebf497b376fdd96c74

---
+++ assets/home-DoYUm3u1.js
@@ -0,0 +1,1 @@
+import{h as o}from"./chunk-JPUPSTYD-B5mLITU0.js";import{w as t,l as e,N as r}from"./chunk-LFPYN7LY-CnU3qFER.js";const p=t(function(){return e.jsx(r,{to:o("/map"),replace:!0})});export{p as default};
--- assets/index-Cu2bb5oO.js
+++
@@ -1,1 +0,0 @@
-import{n as a}from"./index-TkFpP8yd.js";var r=a;export{r as i};
